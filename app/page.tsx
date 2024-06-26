20b5b8eea32dd09c78660d1f406ce68b0ddb354b
khayridine <khayrimizen8@gmail.com>
2024-06-26 15:17:05 +0200

    create new folder

--- app/page.tsx
+++ app/page.tsx
@@ -36,13 +36,16 @@ export default function Home() {
             </h2>
             <div className="border-2 w-10 border-blue-500 inline-block mb-2"></div>
             <div className="flex justify-center mb-2">
-              <a href="#" className="border-2 border-gray-200 rounded-full p-3 mx-1">
+              <a href="https://www.facebook.com/"
+               className="border-2 border-gray-200 rounded-full p-3 mx-1">
                 <FaFacebookF className="text-sm" />
               </a>
-              <a href="#" className="border-2 border-gray-200 rounded-full p-3 mx-1">
+              <a href="https://fr.linkedin.com/"
+               className="border-2 border-gray-200 rounded-full p-3 mx-1">
                 <FaLinkedinIn className="text-sm" />
               </a>
-              <a href="/Google" className="border-2 border-gray-200 rounded-full p-3 mx-1">
+              <a href="https://mail.google.com/mail/u/0/#inbox"
+               className="border-2 border-gray-200 rounded-full p-3 mx-1">
                 <FaGoogle className="text-sm" />
               </a>
             </div>
@@ -75,7 +78,7 @@ export default function Home() {
                 <a href="#" className="text-xs">Forgot Password!</a>
               </div>
               <a
-                href="/"
+                href="#"
                 className="border-2 border-blue-500 text-blue-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-blue-500 hover:text-white"
                >
                 Sign In
@@ -90,13 +93,14 @@ export default function Home() {
           <p className="mb-10">
             Fill up personal information and start with us.
           </p>
-           
+          
           <a
-            href="/"
+            href="Signup/"
             className="border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-blue-500"
           >
             Sign up
           </a>
+         
           
         </div>
         {/* sign up section */}
@@ -105,7 +109,7 @@ export default function Home() {
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left  ">
 
        <a
-  href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+  href="/Signup"
   className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
   target="_blank"
   rel="noopener noreferrer"
